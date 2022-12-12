4a27ebe7f499bdc9ba9bd25517b6cc3f2ac3eb6f
Arlone4u <arlochristian.verallo.21@usjr.edu.ph>
2022-12-13 01:02:21 +0800

    Adding another List

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,5 +1,19 @@
-export default function MovieList() {
-    return <movie className = "movie-list">
+const MovieList = (props) => {
+    return <movie>
+        <div className="title1">
+               <h1 className='col1'>Weekly Top 10</h1>
+        </div>
+        <div className = "movie-list">
+            {props.movies.map((movie,index) => <div><img src={movie.Poster} className = 'row_poster'alt = 'movie'></img> <text className="movie-title">{movie.Title}</text><div><text className="movie-price">{movie.Price}</text></div></div>)}
+        </div>
+        <div className="title2">
+               <h1 className='col2'>Watch Again</h1>
+        </div>
+        <div className = "movie-list">
+            {props.movies2.map((movie2,index) => <div><img src={movie2.Poster} className = 'row_poster'alt = 'movie'></img> <text className="movie-title">{movie2.Title}</text></div>)}
+        </div>
 
     </movie>
 }
+
+export default  MovieList;
